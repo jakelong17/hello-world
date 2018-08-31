8e9898d0a3d410a1f90411115630befa6b4e9f78
jakelong17 <42873605+jakelong17@users.noreply.github.com>
2018-08-31 16:05:31 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jake Long, Luke Downs
+ * Date: 2018/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jake Long, Luke Downs\n");
+
+  return 0;
+}
